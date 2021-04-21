917bdcacff01d1b4b873b290dbe97f91ab6d859e
dawanqu-ai <chenguimin@chaungxin.com>
2021-04-21 13:44:31 +0800

    init

---
+++ ZEN/__init__.py
@@ -0,0 +1,9 @@
+version = "0.1.0"
+
+from .tokenization import BertTokenizer, BasicTokenizer, WordpieceTokenizer, _is_whitespace, whitespace_tokenize, convert_to_unicode, _is_punctuation, _is_control,VOCAB_NAME
+from .optimization import BertAdam, WarmupLinearSchedule, AdamW, get_linear_schedule_with_warmup
+from .schedulers import LinearWarmUpScheduler
+from .modeling import ZenConfig, ZenForPreTraining, ZenForTokenClassification, ZenForSequenceClassification, ZenForQuestionAnswering,ZenModel
+from .file_utils import WEIGHTS_NAME, CONFIG_NAME, PYTORCH_PRETRAINED_BERT_CACHE
+from .ngram_utils import ZenNgramDict, NGRAM_DICT_NAME, extract_ngram_feature, construct_ngram_matrix
+
